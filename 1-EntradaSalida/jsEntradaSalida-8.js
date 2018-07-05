9bae35ffc52bf6fea9de63a4c7d00c5d8b586642
lorenatolaba <lorenatolaba_14@hotmail.com>
2018-07-05 10:55:57 -0300

    jueves1

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,13 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+    var dividendo
+    var divisor
+    var resto
+    var cociente
+    dividendo= document.getElementById("numeroDividendo").value;
+    divisor= document.getElementById("numeroDivisor").value;
+    resto=dividendo%divisor;
+    alert("el resto es" + resto);
 	
 }
