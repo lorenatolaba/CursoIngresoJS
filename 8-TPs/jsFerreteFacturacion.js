ef5522d1feb49fde2f0aaccf8e570630aaa54608
lorenatolaba <lorenatolaba_14@hotmail.com>
2018-07-05 12:16:59 -0300

    jueves 3

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,10 +5,37 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+    var precioUno 
+    var precioDos
+    var precioTres
+    var suma
+   precioUno=document.getElementById("PrecioUno").value;
+   precioDos=document.getElementById("PrecioDos").value;
+   precioTres=document.getElementById("PrecioTres").value;
+   precioUno=parseInt(precioUno);
+   precioDos=parseInt(precioDos);
+   precioTres=parseInt(precioTres);
+   suma=precioUno+precioDos+precioTres;
+   alert(suma);
+    
 	
 }
 function Promedio () 
 {
+    var precioUno ;
+    var precioDos;
+    var precioTres;
+    var promedio;
+   precioUno=document.getElementById("PrecioUno").value;
+   precioDos=document.getElementById("PrecioDos").value;
+   precioTres=document.getElementById("PrecioTres").value;
+   precioUno=parseInt(PrecioUno);
+   precioDos=parseInt(PrecioDos);
+   precioTres=parseInt(PrecioTres);
+   promedio=(PrecioUno+PrecioDos+PrecioTres)/3;
+   alert(promedio);
+  
+
 	
 }
 function PrecioFinal () 
