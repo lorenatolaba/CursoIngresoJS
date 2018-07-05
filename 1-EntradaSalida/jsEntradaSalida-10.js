2885418b78dede9c3efec360f87564d43785f156
lorenatolaba <lorenatolaba_14@hotmail.com>
2018-07-05 11:36:42 -0300

    jueves3

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+    var sueldo;
+    var porcentaje;
+    var resultado;
+    sueldo=document.getElementById("sueldo").value;
+    porcentaje=sueldo*(25/100);
+    sueldo=parseInt(sueldo);
+    resultado=porcentaje+sueldo;
+    document.getElementById("resultado").value=resultdo; 
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    var porsentaje;
+    var resultado;
+    sueldo=document.getElementById("sueldo").value;
+    porcentaje=sueldo*(10/100);
+    sueldo=parseInt(sueldo);
+    resultado=porcentaje+sueldo;
+    document.getElementById("resultado").value=resultado;
+    
+
 
 	
 }
