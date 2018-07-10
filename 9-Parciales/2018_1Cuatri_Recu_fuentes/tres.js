da57c8ff2bc3ee3bb1a6e70d883cfbd234decf10
lorenatolaba <lorenatolaba_14@hotmail.com>
2018-07-10 09:52:07 -0300

    tpss

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+    var precio;
+    var porcentajeDescuento;
+    var descuentoEnDinero;
+    var precioConDescuento;
+    var iva;
+    var precioFinal;
+
+    precio=prompt("ingrese el precio")
+    porcentajeDescuento=prompt("ingrese el porcenaje de descuento");
+
+    precio=parseInt(precio);
+    porcentajeDescuento=parseint(porcentajeDescuento);
+    
+
+
+    
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,28 @@
 
 function mostrar()
 {
+    var base;
+    var altura;
+    var area;
+    var perimetro;
+
+    base=prompt(" ingrese base de un triangulo ");
+    altura=prompt("ingrese altura de un triangulo");
+
+    base=parseInt(base);
+    altura=parseInt(altura);
+    perimetro=base*3;
+    superficie=(base*altura)/3;
+    alert
+
+
+
+    
+    
+    
+    
+    
+    
+
 
 }
