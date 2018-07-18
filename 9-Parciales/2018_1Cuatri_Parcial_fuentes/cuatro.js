5107f059148b315d549bc8e014814ac2cc1f717c
lorena <lorenatolaba_14@hotmail.com>
2018-07-18 18:31:34 -0300

    45

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+    var numeroUno; 
+    var numeroDos;
+    var suma;
+    
+    
+   numeroUno=prompt(" ingrese un numeros");
+   numeroDos=prompt("ingrese un numero");
+   numeroUno=parseInt(numeroUno);
+   numeroDos=parseInt(numeroDos);
+   suma=numeroUno+numeroDos; 
+   if(suma>10){ alert("la suma es "+ suma + "y supero el 10");
+}{}
+   
+   if(numeroUno == numeroDos){ alert( numeroUno + "" + numeroDos);
+} else{
+    if( numeroUno>numeroDos){alert( numeroUno - numeroDos);
+     } else {
+          alert(numeroUno + numeroDos);
+    
+        }
+        
+        }
+     }
 
-}
+   
+   
